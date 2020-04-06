5eb9195b0797d30287ae7f00b75fd6a87bc1c5d8
LinRN <44719485+Linrnn@users.noreply.github.com>
2020-04-06 22:37:24 +0800

    Add files via upload

---
+++ hoi4_modifier/MainForm.Designer.cs
@@ -0,0 +1,117 @@
+﻿using System.Windows.Forms;
+
+public partial class mainForm
+{
+    /// <summary>
+    /// 必需的设计器变量。
+    /// </summary>
+    private System.ComponentModel.IContainer components;
+
+    /// <summary>
+    /// 清理所有正在使用的资源。
+    /// </summary>
+    /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+    protected override void Dispose(bool disposing)
+    {
+        if (disposing && (components != null))
+        {
+            components.Dispose();
+        }
+        base.Dispose(disposing);
+    }
+
+    /// <summary>
+    /// 设计器支持所需的方法 - 不要修改
+    /// 使用代码编辑器修改此方法的内容。
+    /// </summary>
+    private void InitializeComponent()
+    {
+            this.clearButton = new System.Windows.Forms.Button();
+            this.inputRichTextBox = new System.Windows.Forms.RichTextBox();
+            this.modifyButton = new System.Windows.Forms.Button();
+            this.printLabel = new System.Windows.Forms.Label();
+            this.delectButton = new System.Windows.Forms.Button();
+            this.ReadButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // clearButton
+            // 
+            this.clearButton.Location = new System.Drawing.Point(460, 250);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(100, 60);
+            this.clearButton.TabIndex = 1;
+            this.clearButton.Text = "清除";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.ClearButton_Click);
+            // 
+            // inputRichTextBox
+            // 
+            this.inputRichTextBox.Location = new System.Drawing.Point(10, 20);
+            this.inputRichTextBox.Name = "inputRichTextBox";
+            this.inputRichTextBox.Size = new System.Drawing.Size(550, 160);
+            this.inputRichTextBox.TabIndex = 2;
+            this.inputRichTextBox.Text = "";
+            // 
+            // modifyButton
+            // 
+            this.modifyButton.Location = new System.Drawing.Point(350, 250);
+            this.modifyButton.Name = "modifyButton";
+            this.modifyButton.Size = new System.Drawing.Size(100, 60);
+            this.modifyButton.TabIndex = 3;
+            this.modifyButton.Text = "修改";
+            this.modifyButton.UseVisualStyleBackColor = true;
+            this.modifyButton.Click += new System.EventHandler(this.ModifyButton_Click);
+            // 
+            // printLabel
+            // 
+            this.printLabel.AutoSize = true;
+            this.printLabel.Location = new System.Drawing.Point(20, 200);
+            this.printLabel.Name = "printLabel";
+            this.printLabel.Size = new System.Drawing.Size(0, 15);
+            this.printLabel.TabIndex = 6;
+            // 
+            // delectButton
+            // 
+            this.delectButton.Location = new System.Drawing.Point(240, 250);
+            this.delectButton.Name = "delectButton";
+            this.delectButton.Size = new System.Drawing.Size(100, 60);
+            this.delectButton.TabIndex = 7;
+            this.delectButton.Text = "删除";
+            this.delectButton.UseVisualStyleBackColor = true;
+            this.delectButton.Click += new System.EventHandler(this.DelectButton_Click);
+            // 
+            // ReadButton
+            // 
+            this.ReadButton.Location = new System.Drawing.Point(130, 250);
+            this.ReadButton.Name = "ReadButton";
+            this.ReadButton.Size = new System.Drawing.Size(100, 60);
+            this.ReadButton.TabIndex = 8;
+            this.ReadButton.Text = "读取";
+            this.ReadButton.UseVisualStyleBackColor = true;
+            this.ReadButton.Click += new System.EventHandler(this.ReadButton_Click);
+            // 
+            // mainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(582, 353);
+            this.Controls.Add(this.ReadButton);
+            this.Controls.Add(this.delectButton);
+            this.Controls.Add(this.printLabel);
+            this.Controls.Add(this.modifyButton);
+            this.Controls.Add(this.inputRichTextBox);
+            this.Controls.Add(this.clearButton);
+            this.Name = "mainForm";
+            this.Text = "钢铁雄心4文本修改器";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+    }
+
+    private RichTextBox inputRichTextBox;
+    private Button modifyButton;
+    private Button clearButton;
+    private Label printLabel;
+    private Button delectButton;
+    private Button ReadButton;
+}
---
+++ hoi4_modifier/MainForm.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+public partial class mainForm : Form
+{
+    private List<int> _list = new List<int>();
+
+    public mainForm()
+    {
+        InitializeComponent();
+    }
+
+    private void ReadButton_Click(object sender, EventArgs e)
+    {
+        _list.Clear();
+        if (File.Exists(inputRichTextBox.Text))
+        {
+            printLabel.Text = "开始读取";
+
+            StreamReader reader = new StreamReader(inputRichTextBox.Text);
+            while (!reader.EndOfStream)
+            {
+                string s = reader.ReadLine().ToLower();
+                if (Regex.IsMatch(s, "land *; *true")) { _list.Add(int.Parse(Regex.Match(s, @"\d+").Value)); }
+            }
+            reader.Close();
+
+            printLabel.Text = "结束读取";
+        }
+        else
+        {
+            printLabel.Text = "路径不存在";
+        }
+    }
+
+    private void DelectButton_Click(object sender, EventArgs e)
+    {
+        if (Directory.Exists(inputRichTextBox.Text))
+        {
+            printLabel.Text = "开始删除";
+
+            foreach (string path in Directory.GetFiles(inputRichTextBox.Text))
+            {
+                StreamWriter writer = new StreamWriter(path);
+                writer.WriteLine(string.Empty);
+                writer.Close();
+            }
+
+            printLabel.Text = "结束删除";
+        }
+        else
+        {
+            printLabel.Text = "路径不存在";
+        }
+    }
+
+    private void ModifyButton_Click(object sender, EventArgs e)
+    {
+        if (Directory.Exists(inputRichTextBox.Text))
+        {
+            if (_list == null || _list.Count == 0)
+            {
+                printLabel.Text = "未读取\"definition.csv\"文件";
+            }
+            else
+            {
+                printLabel.Text = "开始修改";
+
+                foreach (string path in Directory.GetFiles(inputRichTextBox.Text))
+                {
+                    #region 读取
+                    StreamReader reader = new StreamReader(path);
+                    string text = reader.ReadToEnd();
+                    reader.Close();
+                    string id = Regex.Match(text, @"id\s*=\s*(\d+)").Groups[1].Value;
+                    string manpower = Regex.Match(text, @"manpower\s*=\s*(\d+)").Groups[1].Value;
+                    string state_category = Regex.Match(text, "state_category\\s*=\\s*\"?([_a-z]+)\"?").Groups[1].Value;
+                    string resources = Regex.Match(text, @"resources\s*=\s*{\s*([=#:.\w\s]*)\s*}").Groups[1].Value;
+                    string owner = Regex.Match(text, @"owner\s*=\s*([A-Z]{3})").Groups[1].Value;
+                    MatchCollection victory_points = Regex.Matches(text, @"victory_points\s*=\s*{\s*\d[=#:.\d\s^{}]*\d\s*}");
+                    string provinces = Regex.Match(text, @"provinces\s*=\s*{\s*(\d[\s\d^{}]*\d)\s*}").Groups[1].Value;
+                    #endregion
+
+                    #region 修改
+                    if (Regex.IsMatch("SOV GER ITA POL LIT LAT EST SWI BEL LUX CZE AUS HUN YUG ROM BUL GRE ALB SWE NOR FIN CHI PRC SHX YUN GXC MAN MEN XSM SIK TIB MON TAN RAJ NEP BHU TUR IRQ PER AFG SAU YEM OMA ETH SAF LIB SIA", owner))
+                    {
+                        owner = "LO1";
+                    }
+                    else if (Regex.IsMatch("USA CAN MEX GUA HON ELS NIC COS PAN CUB HAI DOM COL VEN ECU PRU BRA BOL PAR URG ARG CHL IRE PHI INS AST NZL", owner))
+                    {
+                        owner = "LO0";
+                    }
+                    else if (Regex.IsMatch("ENG FRA SPR POR DEN HOL", owner))
+                    {
+                        owner = "LO1";
+                    }
+                    else if (Regex.IsMatch("JAP MAL", owner))
+                    {
+                        owner = "LO0";
+                    }
+                    else
+                    {
+                        owner = "LO1";
+                    }
+                    #endregion
+
+                    #region 写入
+                    StreamWriter writer = new StreamWriter(path);
+                    writer.WriteLine("state =");
+                    writer.WriteLine("{");
+                    writer.WriteLine($"\tid = {id}");
+                    writer.WriteLine($"\tname = \"STATE_{id}\"");
+                    writer.WriteLine($"\tmanpower = {manpower}");
+                    writer.WriteLine($"\tstate_category = {state_category}");
+                    if (Regex.IsMatch(text, "impassable *= *yes")) { writer.WriteLine("\timpassable = yes"); }
+                    if (Regex.IsMatch(text, "resources"))
+                    {
+                        writer.WriteLine("\tresources =");
+                        writer.WriteLine("\t{");
+                        writer.Write($"\t\t{resources}");
+                        writer.WriteLine("}");
+                    }
+                    writer.WriteLine("\thistory =");
+                    writer.WriteLine("\t{");
+                    writer.WriteLine($"\t\towner = {owner}");
+                    bool flag = false;
+                    if (Regex.IsMatch(text, "victory_points"))
+                    {
+                        foreach (Match match in victory_points)
+                        {
+                            string num = match.Value;
+                            writer.WriteLine($"\t\t{num}");
+                            if (!flag && _list.Contains(int.Parse(Regex.Match(num, @"\d+").Value))) { flag = true; }
+                        }
+                    }
+                    if (flag)
+                    {
+                        writer.WriteLine("\t\tbuildings =");
+                        writer.WriteLine("\t\t{");
+                        foreach (Match match1 in victory_points)
+                        {
+                            MatchCollection collection = Regex.Matches(match1.Value, @"[.\d]+");
+                            for (int i = 0; i < collection.Count; i += 2)
+                            {
+                                int p_id = int.Parse(collection[i].Value);
+                                if (_list.Contains(p_id))
+                                {
+                                    int lv = 0;
+                                    switch ((int)float.Parse(collection[i + 1].Value))
+                                    {
+                                        case 1: lv = 1; break;
+                                        case 3: lv = 2; break;
+                                        case 5: lv = 3; break;
+                                        case 10: lv = 4; break;
+                                        case 15: lv = 5; break;
+                                        case 20: lv = 6; break;
+                                        case 25: lv = 7; break;
+                                        case 30: lv = 8; break;
+                                        case 40: lv = 9; break;
+                                        case 50: lv = 10; break;
+                                    }
+                                    if (lv != 0) { writer.WriteLine($"\t\t\t{p_id} = {{ naval_base = {lv} }}"); }
+                                }
+                            }
+                        }
+                        writer.WriteLine("\t\t}");
+                    }
+                    writer.WriteLine("\t\tadd_core_of = LO1");
+                    writer.WriteLine("\t\tadd_core_of = LO0");
+                    writer.WriteLine("\t}");
+                    writer.WriteLine($"\tprovinces = {{ {provinces} }}");
+                    writer.Write("}");
+                    writer.Close();
+                    #endregion
+                }
+
+                printLabel.Text = "结束修改";
+            }
+        }
+        else
+        {
+            printLabel.Text = "路径不存在";
+        }
+    }
+
+    private void ClearButton_Click(object sender, EventArgs e)
+    {
+        inputRichTextBox.Text = null;
+        printLabel.Text = null;
+        if (_list != null) { _list.Clear(); }
+    }
+}
